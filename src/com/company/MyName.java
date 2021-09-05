b823dee8f2872308eddc290907ee0cbd979a1685
Christian Estela <cestela3@gmail.com>
2021-09-05 15:24:21 -0400

    Revision

--- src/com/company/MyName.java
+++ src/com/company/MyName.java
@@ -1,2 +1,12 @@
-package com.company;public class MyName {
+package com.company;
+
+import java.util.Scanner;
+
+public class MyName {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your name?");
+        String name = input.nextLine();
+        System.out.println("Salutations "+name+" ! ");
+    }
 }
